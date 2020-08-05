6ae2f74964c5137daed7b74c122831956c44c01b
jcanz <3466642+jcanzonetta@users.noreply.github.com>
2020-08-04 18:17:07 -0700

    Add happy path scripts for calcuations
    First time undoing a local commit. I used "git reset HEAD~1" to keep my modified files, but revoke the commit.

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+const resultField = document.querySelector("#result-field")
+const numButtons = document.querySelectorAll(".numButton")
+const operateButtons = document.querySelectorAll(".operateButton")
+const equateButton = document.querySelector("#equate-button")
+let displayedNumber = ""
+let savedNumber;
+let operator;
+
+// Number Button Event Listeners
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayedNumber += button.innerHTML
+        resultField.innerHTML = displayedNumber
+    })
+})
+
+//  Operator Button Event Listeners
+operateButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operator = button.innerHTML
+        savedNumber = displayedNumber
+        displayedNumber = ""
+    })
+})
+
+// Operate/Equals Event listener
+equateButton.addEventListener('click', () =>{
+    resultField.innerHTML = operate(operator, savedNumber, displayedNumber)
+})
+
+
+
+
+function operate(operator, a, b){
+    a = parseInt(a)
+    b = parseInt(b)
+    
+    switch (operator){
+        case "+":
+            console.log(`savedNumber: ${a}`)
+            console.log(`displayedNumber: ${b}`)
+            console.log(`computation: ${add(a, b)}`)
+            return add(a, b)
+            break
+        case "-":
+            return subtract(a,b)
+            break
+        case "÷":
+            return divide(a,b )
+            break
+        case "x":
+            return multiply(a, b)
+            break
+        default:
+            console.error(`Operate function failed with operator: ${operator}`)
+    }
+}
+
+// Operator Functions
 function add(a, b) {
 	return a + b
 }
@@ -13,23 +72,3 @@ function multiply(a,b) {
 function divide(a,b){
     return a/b
 }
-
-function operate(operator, a, b){
-    switch (operator){
-        case "+":
-            return add(a, b)
-            break
-        case "-":
-            return substract(a,b)
-            break
-        case "%":
-            return divide(a,b )
-            break
-        case "x":
-            return multiply(a, b)
-            break
-        default:
-            console.error("This operator is not supported yet.")
-    }
-}
-
